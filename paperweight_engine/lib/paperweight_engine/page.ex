06372c41e626638dde5aa9385bf6a22ccac56c63
PerplexedSphex <sam.x.jacobs@gmail.com>
2018-02-07 17:02:43 -0800

    cleanuped directory

--- paperweight_engine/lib/paperweight_engine/page.ex
+++ paperweight_engine/lib/paperweight_engine/page.ex
@@ -1,10 +1,14 @@
 defmodule PaperweightEngine.Page do
     alias __MODULE__
+    alias PaperweightEngine.Bullet
 
     @enforce_keys [:bullet, :level, :mode]
     defstruct [:bullet, :level, :mode]
 
     def new(), do:
-        %Page{bullet: %Bullet{}, level: 0, mode: :insert}
+        %Page{bullet: Bullet.new(), level: 0, mode: :insert}
+
+    def toggle_mode(%Page = page):
+        %{Page | mode: :command}
 
 end
